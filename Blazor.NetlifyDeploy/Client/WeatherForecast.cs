a3d4748a2be56defebd7de8005b84a2c3762dee8
zapsocialapp <91618776+zapsocialapp@users.noreply.github.com>
2021-09-30 09:28:07 -0500

    Add files via upload

---
+++ Blazor.NetlifyDeploy/Client/WeatherForecast.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Blazor.NetlifyDeploy.Client
+{
+    public class WeatherForecast
+    {
+        public DateTime ApprovedTime { get; set; }
+        public DateTime ReferenceTime { get; set; }
+        public Geometry Geometry { get; set; }
+        public Timesery[] TimeSeries { get; set; }
+    }
+
+    public class Geometry
+    {
+        public string Type { get; set; }
+        public float[][] Coordinates { get; set; }
+    }
+
+    public class Timesery
+    {
+        public DateTime ValidTime { get; set; }
+        public Parameter[] Parameters { get; set; }
+    }
+
+    public class Parameter
+    {
+        public string Name { get; set; }
+        public string LevelType { get; set; }
+        public int Level { get; set; }
+        public string Unit { get; set; }
+        public float[] Values { get; set; }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Blazor
+# Blazor.NetlifyDeploy
+Example code for hosting and deploying a Blazor Application for Netlify
